406e48f6291b3a1de8757a1cd25cc0de3172f442
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-11-08 00:59:00 +0600

    problem 4 solve

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -1,14 +1,3 @@
-// Create a TypeScript function called countWordOccurrences that accepts a sentence (string) and a word (string). The function should return the number of times the word appears in the sentence, ignoring case. Use the simple sentence without punctuation as input.
-
-// Example:
-
-// Input: "I love typescript", "typescript"
-// Output: 1
-// // Sample Input:
-// countWordOccurrences("I love typescript", "typescript");
-
-// // Sample Output:
-// 1;
 
 const countWordOccurrences = (sentence: string, word: string) => {
     let count = 0;
--- src/problem-4.ts
+++ src/problem-4.ts
@@ -1,17 +1,19 @@
-// Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
+type Circle = {
+    shape: 'circle';
+    radius: number
+};
 
-// // Sample Input 1:
-// const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+type Rectangle = {
+    shape: 'rectangle';
+    width: number;
+    height: number
+}
 
-// // Sample Output 1:
-// 78.54;
-
-// // Sample Input 2:
-// const rectangleArea = calculateShapeArea({
-//   shape: "rectangle",
-//   width: 4,
-//   height: 6,
-// });
-
-// // Sample Output 2:
-// 24;
+const calculateShapeArea = (shape: Circle | Rectangle) => {
+    if (shape.shape === "circle") {
+        return Math.PI * shape.radius * shape.radius;
+    }
+    if (shape.shape === 'rectangle') {
+        return shape.height * shape.width;
+    }
+}
