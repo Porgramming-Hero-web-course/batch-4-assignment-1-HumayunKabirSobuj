3d5e9872d2155c09a86ef5201e71ae3f3e40674a
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-11-08 15:13:02 +0600

    problem 7 ans modify

--- src/problem-7.ts
+++ src/problem-7.ts
@@ -4,7 +4,7 @@ class Car{
     constructor(public make:string,public model:string,public year:number){}
     getCarAge(){
         const currentYear= new Date().getFullYear();
-        return currentYear - this.year;
+        return `${currentYear - this.year} (assuming current year is 2024)`;
     }
 }
 
